21c7dc43f16a41bf2b4dc6e3ccd0ce62fd902aa5
mrodazo617 <mrodazo617@g.educaand.es>
2023-12-20 17:39:52 +0100

    Ejercicios U2

--- Ejercicios/src/main/kotlin/EjerciciosU2.kt
+++ Ejercicios/src/main/kotlin/EjerciciosU2.kt
@@ -6,7 +6,6 @@
  */
 
 
-
 /**
  * Ejercicio 2.1.2
  * Escribir un programa que almacene la cadena de caracteres contraseña en una variable,
@@ -32,8 +31,7 @@ fun ejercicio202() {
         if (comparaContrasena(passw1, passw2)) {
             println("Contraseña correcta")
             intento = false
-        }
-        else {
+        } else {
             println("Contraseña incorrecta")
         }
     }
@@ -45,11 +43,11 @@ fun ejercicio202() {
  * Si el divisor es cero el programa debe mostrar un error.
  */
 
-fun division (num1: Int, num2: Int): Float {
-    return (num1.toFloat()/num2.toFloat())
+fun division(num1: Int, num2: Int): Float {
+    return (num1.toFloat() / num2.toFloat())
 }
 
-fun ejercicio203(){
+fun ejercicio203() {
 
     println("Introduce el dividendo: ")
     val dividendo = readln().toInt()
@@ -57,11 +55,103 @@ fun ejercicio203(){
     println("Introduce el divisor: ")
     val divisor = readln().toInt()
 
-    if (divisor!=0){
-        println("El resultado es ${"(%.2f)".format(division(dividendo,divisor))}")
-    }
-    else {
+    if (divisor != 0) {
+        println("El resultado es ${"(%.2f)".format(division(dividendo, divisor))}")
+    } else {
         println("**ERROR** El divisor es 0, introduzca otra división")
         main()
     }
+}
+
+fun esLetraAnterior(letra_a: String, letra_b: String): Boolean {
+    return letra_a < letra_b
+}
+
+fun perteneceGrupo(sexo: String, nombre: String): String {
+
+    val inicialNombre = nombre[0].uppercase()
+
+    if (
+        (sexo.lowercase() == "hombre" && esLetraAnterior("N", inicialNombre))
+        ||
+        (sexo.lowercase() == "mujer" && esLetraAnterior(inicialNombre, "M"))
+    ) {
+        return "A"
+    }
+    return "B"
+}
+
+/**
+ * Otra forma de hacer la función pero quitando código. Cuando sólo tiene que devolver un valor,
+ * hacer sólo un cálculo, es mejor así ya que If y Else devuelven un valor, por eso no se pone return.
+ * Lo que siempre hay que decirle si no es una cosa, que es entonces (else).
+ */
+fun perteneceGrupo1(sexo: String, nombre: String) =
+    if (
+        (sexo.lowercase() == "hombre" && esLetraAnterior("N", nombre[0].uppercase()))
+        ||
+        (sexo.lowercase() == "mujer" && esLetraAnterior(nombre[0].uppercase(), "M"))
+    ) {
+        "A"
+    } else {
+        "B"
+    }
+
+/**
+ * Ejercicio 2.1.6
+ * Los alumnos de un curso se han dividido en dos grupos A y B de acuerdo al sexo y el nombre.
+ * El grupo A esta formado por las mujeres con un nombre anterior a la M y los hombres con un
+ * nombre posterior a la N y el grupo B por el resto. Escribir un programa que pregunte al
+ * usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde.
+ */
+
+fun ejercicio206() {
+
+    println("Introduce tu nombre: ")
+    val nombre = readln()
+
+    println("Introduce tu sexo (mujer/hombre): ")
+    val sexo = readln()
+
+    println("Usted pertenece al grupo: ${perteneceGrupo1(sexo, nombre)}")
+
+}
+
+fun comprobarPuntuacion(puntos: Double) =
+        ((puntos == 0.0) || (puntos == 0.4) || (puntos >= 0.6))
+
+fun nivel (puntos: Double) : String {
+    if (puntos == 0.0) {
+        return "Nivel inaceptable"
+    }
+    else if (puntos == 0.4) {
+        return "Nivel aceptable"
+    }
+    else if (puntos >= 0.6) {
+        return 'Nivel meritorio'
+    }
+    else {
+
+    }
+
+}
+
+/**
+ * Ejercicio 2.1.8
+ * En una determinada empresa, sus empleados son evaluados al final de cada año.
+ * Los puntos que pueden obtener en la evaluación comienzan en 0.0 y pueden ir aumentando,
+ * traduciéndose en mejores beneficios. Los puntos que pueden conseguir los empleados pueden
+ * ser 0.0, 0.4, 0.6 o más, pero no valores intermedios entre las cifras mencionadas.
+ * A continuación se muestra una tabla con los niveles correspondientes a cada puntuación.
+ * La cantidad de dinero conseguida en cada nivel es de 2.400€ multiplicada por la puntuación del nivel.
+ *
+ * Nivel Inaceptable --> Puntuación 0.0
+ * Nivel Aceptable --> Puntuación 0.4
+ * Nivel Meritorio --> Puntuación 0.6 o más
+ */
+
+fun ejercicio208 (){
+
+
+
 }
