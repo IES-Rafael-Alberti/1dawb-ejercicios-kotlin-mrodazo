5f4372ec65641852f890386e156352c1df883c7b
mrodazo617 <mrodazo617@g.educaand.es>
2024-01-12 21:37:21 +0100

    Ejercicio 331

---
+++ Ejercicios/src/main/kotlin/EjerciciosU3.kt
@@ -0,0 +1,52 @@
+
+/**
+ * Ejercicio 3.3.1¶
+ * Suponer una lista con datos de las compras hechas por clientes de una empresa a lo largo de un mes, la cual contiene tuplas con información de cada venta: (cliente, día del mes, monto, domicilio del cliente). Ejemplo:
+ *
+ * [("Nuria Costa", 5, 12780.78, "Calle Las Flores 355"), ("Jorge Russo", 7, 699, "Mirasol 218"), ("Nuria Costa", 7, 532.90, "Calle Las Flores 355"), ("Julián Rodriguez", 12, 5715.99, "La Mancha 761"), ("Jorge Russo", 15, 958, "Mirasol 218")]
+ * Escribir una función que reciba como parámetro una lista con el formato mencionado anteriormente y retorne los domicilios de cada cliente al cual se le debe enviar una factura de compra. Notar que cada cliente puede haber hecho más de una compra en el mes, por lo que la función debe retornar una estructura que contenga cada domicilio una sola vez.
+ */
+
+fun ejercicio331 () {
+
+    /*
+    Una forma:
+    val listaCompras = mutableListOf<List<Any>>()
+
+    listaCompras.add (listof("Nuria Costa", 5, 12780.78, "Calle Las Flores 355"))
+    listaCompras.add (listof("Jorge Russo", 7, 699, "Mirasol 218"))
+    listaCompras.add (listof("Nuria Costa", 5, 12780.78, "Calle Las Flores 355"))
+    listaCompras.add (listof("Julián Rodriguez", 12, 5715.99, "La Mancha 761"))
+    listaCompras.add (listof("Jorge Russo", 15, 958, "Mirasol 218))
+
+     */
+
+    val listaCompras: List<List<Any>> = listOf(
+        listOf("Nuria Costa", 5, 12780.78, "Calle Las Flores 355"),
+        listOf("Jorge Russo", 7, 699, "Mirasol 218"),
+        listOf("Nuria Costa", 5, 12780.78, "Calle Las Flores 355"),
+        listOf("Julián Rodriguez", 12, 5715.99, "La Mancha 761"),
+        listOf("Jorge Russo", 7, 699, "Mirasol 218")
+    )
+
+    println(domicilios(listaCompras).joinToString(", "))
+}
+
+fun domicilios(compras: List<List<Any>>) : List<String> {
+
+    /**Una forma:
+     * val listaDomicilios = mutableListOf<String>() //Mutable porque la lista se crea con cada iteración
+     *     compras.forEach { elemento -> listaDomicilios.add(elemento[3].toString()) }
+     *     /**Te coge todos los elementos de la lista y los pasa a conjunto, como los conjuntos no tienen
+     *     elementos duplicados. Después lo pasamos a una lista porque hemos dicho que la salida
+     *      fuera una lista.
+     *      Con sorted se ordena la lista
+     *      */
+     *     return listaDomicilios.toSet().toList().sorted()
+     *
+     *     Otra, usando el map que coge la lista ygenera otra cogiendo la info entre las llaves
+     */
+
+    return compras.map{ it[3].toString() }.toSet().toList().sorted()
+
+}
--- Ejercicios/src/main/kotlin/Main.kt
+++ Ejercicios/src/main/kotlin/Main.kt
@@ -43,121 +43,136 @@ fun main() {
      * en mayúscula. El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
      */
 
-     //ejercicio18()
+    //ejercicio18()
 
-     /**
-      * Los teléfonos de una empresa tienen el siguiente formato prefijo-número-extension donde
-      * el prefijo es el código del país +34, y la extensión tiene dos dígitos (por ejemplo +34-913724710-56).
-      * Escribir un programa que pregunte por un número de teléfono con este formato y muestre por pantalla el
-      * número de teléfono sin el prefijo y la extensión.
-      */
+    /**
+     * Los teléfonos de una empresa tienen el siguiente formato prefijo-número-extension donde
+     * el prefijo es el código del país +34, y la extensión tiene dos dígitos (por ejemplo +34-913724710-56).
+     * Escribir un programa que pregunte por un número de teléfono con este formato y muestre por pantalla el
+     * número de teléfono sin el prefijo y la extensión.
+     */
 
-     //ejercicio20()
-
-
-     /**
-      * Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida
-      */
-
-     //ejercicio21()
+    //ejercicio20()
 
 
-     /**
+    /**
+     * Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida
+     */
+
+    //ejercicio21()
+
+
+    /**
      * Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal,
-      * y después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula.
-      */
+     * y después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula.
+     */
 
-     //ejercicio22()
-
-
-     /**
-      * Escribir un programa que pregunte por consola el precio de un producto en euros con dos decimales
-      * y muestre por pantalla el número de euros y el número de céntimos del precio introducido.
-      */
-
-     //ejercicio24()
+    //ejercicio22()
 
 
-     /**
-      * Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato dd/mm/aaaa
-      * y muestra por pantalla, el día, el mes y el año. Adaptar el programa anterior para que también funcione
-      * cuando el día o el mes se introduzcan con un solo carácter.
-      */
+    /**
+     * Escribir un programa que pregunte por consola el precio de un producto en euros con dos decimales
+     * y muestre por pantalla el número de euros y el número de céntimos del precio introducido.
+     */
 
-     //ejercicio25()
-
-     /**
-      * Escribir un programa que pregunte por consola por los productos de una cesta de la compra,
-      * separados por comas, y muestre por pantalla cada uno de los productos en una línea distinta.
-      */
-
-     //ejercicio26()
-
-     /**
-      * Escribir un programa que pregunte el nombre el un producto, su precio y un número de unidades y muestre
-      * por pantalla una cadena con el nombre del producto seguido de su precio unitario con 6 dígitos enteros y 2 decimales,
-      * el número de unidades con tres dígitos y el coste total con 8 dígitos enteros y 2 decimales.
-      */
-
-     //ejercicio27()
+    //ejercicio24()
 
 
-/**
- * U2:
- *   Sentencias condicionales: 2, 3, 6, 8 y 10.
- *   Sentencias iterativas: 2, 4, 6, 7, 8, 13, 15, 18, 19 y 25.
- *   Captura de excepciones: 2, 3 y 4.
- */
+    /**
+     * Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato dd/mm/aaaa
+     * y muestra por pantalla, el día, el mes y el año. Adaptar el programa anterior para que también funcione
+     * cuando el día o el mes se introduzcan con un solo carácter.
+     */
 
-     /**
-      * Ejercicio 2.1.2
-      * Escribir un programa que almacene la cadena de caracteres contraseña en una variable,
-      * pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el
-      * usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
-      */
+    //ejercicio25()
 
-     //ejercicio202()
+    /**
+     * Escribir un programa que pregunte por consola por los productos de una cesta de la compra,
+     * separados por comas, y muestre por pantalla cada uno de los productos en una línea distinta.
+     */
 
+    //ejercicio26()
 
-     /**
-      * Ejercicio 2.1.3
-      * Escribir un programa que pida al usuario dos números y muestre por pantalla su división.
-      * Si el divisor es cero el programa debe mostrar un error.
-      */
+    /**
+     * Escribir un programa que pregunte el nombre el un producto, su precio y un número de unidades y muestre
+     * por pantalla una cadena con el nombre del producto seguido de su precio unitario con 6 dígitos enteros y 2 decimales,
+     * el número de unidades con tres dígitos y el coste total con 8 dígitos enteros y 2 decimales.
+     */
 
-     //ejercicio203()
+    //ejercicio27()
 
 
-     /**
-      * Ejercicio 2.1.6
-      * Los alumnos de un curso se han dividido en dos grupos A y B de acuerdo al sexo y el nombre.
-      * El grupo A esta formado por las mujeres con un nombre anterior a la M y los hombres con un
-      * nombre posterior a la N y el grupo B por el resto. Escribir un programa que pregunte al
-      * usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde.
-      */
+    /**
+     * U2:
+     *   Sentencias condicionales: 2, 3, 6, 8 y 10.
+     *   Sentencias iterativas: 2, 4, 6, 7, 8, 13, 15, 18, 19 y 25.
+     *   Captura de excepciones: 2, 3 y 4.
+     */
 
-     //ejercicio206 ()
+    /**
+     * Ejercicio 2.1.2
+     * Escribir un programa que almacene la cadena de caracteres contraseña en una variable,
+     * pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el
+     * usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
+     */
 
-
-     /**
-      * Ejercicio 2.1.8
-      * En una determinada empresa, sus empleados son evaluados al final de cada año.
-      * Los puntos que pueden obtener en la evaluación comienzan en 0.0 y pueden ir aumentando,
-      * traduciéndose en mejores beneficios. Los puntos que pueden conseguir los empleados pueden
-      * ser 0.0, 0.4, 0.6 o más, pero no valores intermedios entre las cifras mencionadas.
-      * A continuación se muestra una tabla con los niveles correspondientes a cada puntuación.
-      * La cantidad de dinero conseguida en cada nivel es de 2.400€ multiplicada por la puntuación del nivel.
-      *
-      * Nivel Inaceptable --> Puntuación 0.0
-      * Nivel Aceptable --> Puntuación 0.4
-      * Nivel Meritorio --> Puntuación 0.6 o más
-      */
-
-     //ejercicio208 ()
+    //ejercicio202()
 
 
+    /**
+     * Ejercicio 2.1.3
+     * Escribir un programa que pida al usuario dos números y muestre por pantalla su división.
+     * Si el divisor es cero el programa debe mostrar un error.
+     */
+
+    //ejercicio203()
 
 
-     ejercicio2125 ()
+    /**
+     * Ejercicio 2.1.6
+     * Los alumnos de un curso se han dividido en dos grupos A y B de acuerdo al sexo y el nombre.
+     * El grupo A esta formado por las mujeres con un nombre anterior a la M y los hombres con un
+     * nombre posterior a la N y el grupo B por el resto. Escribir un programa que pregunte al
+     * usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde.
+     */
+
+    //ejercicio206 ()
+
+
+    /**
+     * Ejercicio 2.1.8
+     * En una determinada empresa, sus empleados son evaluados al final de cada año.
+     * Los puntos que pueden obtener en la evaluación comienzan en 0.0 y pueden ir aumentando,
+     * traduciéndose en mejores beneficios. Los puntos que pueden conseguir los empleados pueden
+     * ser 0.0, 0.4, 0.6 o más, pero no valores intermedios entre las cifras mencionadas.
+     * A continuación se muestra una tabla con los niveles correspondientes a cada puntuación.
+     * La cantidad de dinero conseguida en cada nivel es de 2.400€ multiplicada por la puntuación del nivel.
+     *
+     * Nivel Inaceptable --> Puntuación 0.0
+     * Nivel Aceptable --> Puntuación 0.4
+     * Nivel Meritorio --> Puntuación 0.6 o más
+     */
+
+    //ejercicio208 ()
+
+
+    ejercicio2125()
+
+
+    /** UD3
+     *
+     */
+
+
+    /**
+     * Ejercicio 3.3.1¶
+     * Suponer una lista con datos de las compras hechas por clientes de una empresa a lo largo de un mes, la cual contiene tuplas con información de cada venta: (cliente, día del mes, monto, domicilio del cliente). Ejemplo:
+     *
+     * [("Nuria Costa", 5, 12780.78, "Calle Las Flores 355"), ("Jorge Russo", 7, 699, "Mirasol 218"), ("Nuria Costa", 7, 532.90, "Calle Las Flores 355"), ("Julián Rodriguez", 12, 5715.99, "La Mancha 761"), ("Jorge Russo", 15, 958, "Mirasol 218")]
+     * Escribir una función que reciba como parámetro una lista con el formato mencionado anteriormente y retorne los domicilios de cada cliente al cual se le debe enviar una factura de compra. Notar que cada cliente puede haber hecho más de una compra en el mes, por lo que la función debe retornar una estructura que contenga cada domicilio una sola vez.
+     */
+
+    ejercicio331()
+
+
 }
-
